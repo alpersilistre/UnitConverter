ee7b8b178599ca1205ad2b2cf2f25b0bf0d24fde
Alper Silistre <alpersilistre@gmail.com>
2017-12-06 02:02:18 +0300

    Add project files.

---
+++ UnitConverter/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnitConverter.Models;
+
+namespace UnitConverter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UnitConverter/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿const metricSelect = $("#metricSelect");
+const imperialSelect = $("#imperialSelect");
+const fromInput = $("#fromInput");
+const toInput = $("#toInput");
+const resultScreen = $("#resultScreen");
+
+let decideConversionMethod = (value) => {
+    let metricValue = metricSelect.val();
+    let imperialValue = imperialSelect.val();
+
+    let result = undefined;
+
+    switch (metricValue) {
+    case "meter":
+        switch (imperialValue) {
+        case "inch":
+            result = meterToInch(value);
+            break;
+        case "foot":
+            result = meterToFoot(value);
+            break;
+        case "yard":
+            result = meterToYard(value);
+            break;
+        case "mile":
+            result = meterToMile(value);
+            break;
+        default:
+        }
+    default:
+    }
+
+    return result;
+};
+
+let meterToInch = (x) => { return Number(x) * 39.3700; };
+let meterToFoot = (x) => { return Number(x) * 3.2808; };
+let meterToYard = (x) => { return Number(x) * 1.0936; };
+let meterToMile = (x) => { return Number(x) * 0.0006; };
+
+metricSelect.change(function () {
+
+    let result;
+    fromInput.val() !== "" ? result = decideConversionMethod(fromInput.val()) : result = "";
+
+    toInput.text(result);
+    resultScreen.text("Result is " + result);
+});
+
+imperialSelect.change(function () {
+
+    let result;
+    fromInput.val() !== "" ? result = decideConversionMethod(fromInput.val()) : result = "";
+
+    toInput.text(result);
+    resultScreen.text("Result is " + result);
+});
+
